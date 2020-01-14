b0ad4b135cc11b3dee34606046faa6434d511208
Sahil Soni <sahilsoni@Sahils-MacBook-Air.local>
2020-01-14 23:44:48 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello 
+          SAHIL SONI FIRST APP CI/CD 
         </p>
         <a
           className="App-link"
